81e2070d0b0a379de05a27e0fae4a285fbf66305
SeekerRook <xrist.vardakis@gmail.com>
2024-01-15 21:39:33 +0200

    Weighted PP

--- KarmadaPN/PNS/Propagation.py
+++ KarmadaPN/PNS/Propagation.py
@@ -21,8 +21,18 @@ def PP_DuplicatedPN (name,cluster_nmber:int=2):
 def  PP_AggregatedPN(name):
     pass
 
-def  PP_StaticWeightsPN(name):
-    pass
+def  PP_StaticWeightsPN(name,cluster_nmber:int=2):
+    pn = PNComponent(name)
+
+    pn.add_place(Place("Services"))
+
+    pn.add_transition(Transition("Propagate"))
+    pn.add_input("Services","Propagate",Variable("svc"))# svc = (Pod,(c1,c2w...),r)
+    for i in range(cluster_nmber):
+        pn.add_place(Place(f"C{i+1}"))
+        pn.add_output(f"C{i+1}","Propagate",Expression(f"(svc[0],round((svc[1][{i}]/(sum(svc[1])))*svc[2]))"))      
+    return pn
+
 
 def  PP_DynamicWeightsPN(name):
     pass
