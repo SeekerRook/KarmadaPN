553123aab0a0222dcda96245821b38c1304691ca
SeekerRook <xrist.vardakis@gmail.com>
2024-03-15 22:04:56 +0200

    Module Structure and moved tests to directory

--- Makefile
+++ Makefile
@@ -1,21 +1,21 @@
 
 all : clean cluster duplicated static dynamic aggregated multipolicy full
 cluster:
-	python test_cluster.py
+	python tests/test_cluster.py
 duplicated:
-	python test_duplicate_pp.py
+	python tests/test_duplicate_pp.py
 static:
-	python test_static_weights_pp.py
+	python tests/test_static_weights_pp.py
 dynamic:
-	python test_dynamic_weights_pp.py
+	python tests/test_dynamic_weights_pp.py
 aggregated:
-	python test_aggregated_pp.py
+	python tests/test_aggregated_pp.py
 multipolicy:
-	python test_multi_policy.py
+	python tests/test_multi_policy.py
 full:
-	python test_all_policies.py
+	python tests/test_all_policies.py
 real:
-	python test_real.py
+	python tests/test_real.py
 clean:
 	rm -f *.png*
 	rm -f *.pkl*
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='KarmadaPN',  # Replace with your desired package name
+  version='0.1.0',  # Start with version 1.0.0 for initial release
+  packages=find_packages(),  # Automatically finds packages
+  include_package_data=True,  # Includes data files
+  license='GPL',  # Replace with your chosen license
+  description='A tool modelling of Multi-Cluster Infrastractures based on Karmada using PetriNets',
+  long_description=open('./README.md').read(),  # Reads description from README
+  long_description_content_type='text/markdown',  # Sets content type for README
+  # Optional arguments
+  install_requires=[  # List of dependencies your package needs
+      'SNAKES',
+      'numpy',
+      'networkx',
+      
+
+  ],
+  
+  
+  classifiers=[  # Classify your package (optional)
+      ],
+)
+
