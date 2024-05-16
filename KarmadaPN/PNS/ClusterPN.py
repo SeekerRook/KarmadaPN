aa277b6c1d96cfc5a8d58f057bc8d3c7ecc5cf40
SeekerRook <xrist.vardakis@gmail.com>
2024-05-16 18:36:29 +0300

    removed legacy from clusterPN

--- KarmadaPN/PNS/ClusterPN.py
+++ KarmadaPN/PNS/ClusterPN.py
@@ -9,30 +9,30 @@ from os import system
 
 
 def MultiNodeClusterPN(name,pending=[],allocated=[],available=[],running=[],mode=""):
-    if mode == "legacy": #TODO remove
-        pn = PNComponent(name)
-        # pn.globals.append("import KarmadaPN.PNS.ClusterPN")
+    # if mode == "legacy": #TODO remove
+    #     pn = PNComponent(name)
+    #     # pn.globals.append("import KarmadaPN.PNS.ClusterPN")
 
 
-        #Places
-        pn.add_place(Place("Pending"))
-        pn.add_place(Place("Running"))
-        pn.add_place(Place("Allocated_Resources"))
-        pn.add_place(Place("Available_Resources"))
-        #Transitions
+    #     #Places
+    #     pn.add_place(Place("Pending"))
+    #     pn.add_place(Place("Running"))
+    #     pn.add_place(Place("Allocated_Resources"))
+    #     pn.add_place(Place("Available_Resources"))
+    #     #Transitions
 
-        # pn.add_transition(Transition("In-Cluster_Placement",Expression("KarmadaPN.ClusterPN.f(pod,mincpu,maxcpu,minram,maxram,Ncpu,ncpu,Nram,nram,Nid,nid)")))
-        pn.add_transition(Transition("In-Cluster_Placement",Expression("nid==Nid and minram <= Nram-nram and mincpu <= Ncpu-ncpu")))
+    #     # pn.add_transition(Transition("In-Cluster_Placement",Expression("KarmadaPN.ClusterPN.f(pod,mincpu,maxcpu,minram,maxram,Ncpu,ncpu,Nram,nram,Nid,nid)")))
+    #     pn.add_transition(Transition("In-Cluster_Placement",Expression("nid==Nid and minram <= Nram-nram and mincpu <= Ncpu-ncpu")))
 
 
-        pn.add_input("Pending","In-Cluster_Placement",Tuple([Variable("pod"), Variable("mincpu"), Variable("maxcpu"), Variable("minram"), Variable("maxram")]))
-        pn.add_input("Available_Resources","In-Cluster_Placement",Tuple([Variable("Nid"),Variable("Nram"),Variable("Ncpu")]))
-        pn.add_input("Allocated_Resources","In-Cluster_Placement",Tuple([Variable("nid"),Variable("nram"),Variable("ncpu")]))
-        pn.add_output("Running","In-Cluster_Placement",Tuple([Variable("pod"), Variable("mincpu"), Variable("maxcpu"), Variable("minram"), Variable("maxram")]))
-        pn.add_output("Available_Resources","In-Cluster_Placement",Tuple([Expression("Nid"),Expression("Nram"),Expression("Ncpu")]))
-        pn.add_output("Allocated_Resources","In-Cluster_Placement",Tuple([Expression("nid"),Expression("nram+minram"),Expression("ncpu+mincpu")]))
-        return pn
-    else:
+    #     pn.add_input("Pending","In-Cluster_Placement",Tuple([Variable("pod"), Variable("mincpu"), Variable("maxcpu"), Variable("minram"), Variable("maxram")]))
+    #     pn.add_input("Available_Resources","In-Cluster_Placement",Tuple([Variable("Nid"),Variable("Nram"),Variable("Ncpu")]))
+    #     pn.add_input("Allocated_Resources","In-Cluster_Placement",Tuple([Variable("nid"),Variable("nram"),Variable("ncpu")]))
+    #     pn.add_output("Running","In-Cluster_Placement",Tuple([Variable("pod"), Variable("mincpu"), Variable("maxcpu"), Variable("minram"), Variable("maxram")]))
+    #     pn.add_output("Available_Resources","In-Cluster_Placement",Tuple([Expression("Nid"),Expression("Nram"),Expression("Ncpu")]))
+    #     pn.add_output("Allocated_Resources","In-Cluster_Placement",Tuple([Expression("nid"),Expression("nram+minram"),Expression("ncpu+mincpu")]))
+    #     return pn
+    # else:
         pn = PNComponent(name)
         pn.globals.append("from KarmadaPN.Functions import Update")
         pn.globals.append("from KarmadaPN.Functions import Add")
