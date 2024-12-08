07b86c6b5a89ca5ce8a73ad750a65e9dcd2804e2
SeekerRook <xrist.vardakis@gmail.com>
2024-12-09 00:12:19 +0200

    test to calculate remainer before scaling for dynamic

--- KarmadaPN/PNS/Propagation.py
+++ KarmadaPN/PNS/Propagation.py
@@ -195,10 +195,10 @@ def  PP_DynamicWeightsPN(name,cluster_number:int=2,method="resourceaware"):
 
             pn.add_place(Place(f"C{i+1}_Resource_Modeling"))
             pn.add_input(f"C{i+1}_Resource_Modeling","Propagate",Variable(f"c{i+1}"))
-            pn.add_output(f"C{i+1}_Resource_Modeling","Propagate",Expression(f"""Update_rm(c{i+1},svc[0],fd(svc,{clusters},{i+1})-r{i+1}-R{i+1})"""))
+            pn.add_output(f"C{i+1}_Resource_Modeling","Propagate",Expression(f"""Update_rm(c{i+1},svc[0],fd((svc[0],svc[-1]-R{i+1}),{clusters},{i+1})-r{i+1}-R{i+1})"""))
 
             pn.add_place(Place(f"C{i+1}"))
-            pn.add_output(f"C{i+1}","Propagate",Expression(f"(svc[0],fd(svc,{clusters},{i+1})-R{i+1},R{i+1})"))   
+            pn.add_output(f"C{i+1}","Propagate",Expression(f"(svc[0],fd((svc[0],svc[-1]-R{i+1}),{clusters},{i+1}),R{i+1})"))   
             pn.add_input(f"C{i+1}","Propagate",Tuple([Variable(f"s{i+1}"),Variable(f"r{i+1}"),Variable(f"R{i+1}")]))    
 
         return pn    
